6e0898dc80fa1ccc7b913dd1bd073daece2b24a7
Mohamed mohsen <36299904+Mohamedmohsenn@users.noreply.github.com>
2021-05-23 16:12:42 +0200

    Add files via upload
    A program includes GUI to guess the next word which has high probability to be choosen using trigram and nltk.
    The dataset we use in khaleej-2004 which includes bahraini sports news.
    You will need to extract the file Khaleej-2004-utf8.zip.

---
+++ Guessing_next_word/temp.py
@@ -0,0 +1,61 @@
+#bsm elah l r7man l r7em
+from tkinter import * 
+
+from nltk.corpus import PlaintextCorpusReader
+
+path = "Khaleej-2004/Sports/"
+corpusReader = PlaintextCorpusReader(path, ".*")
+tokens = corpusReader.words()
+
+
+def getCounterOfWords(userInput,tokens,dic) : 
+    counter = 0
+    myList = userInput.split(" ")
+    
+    for i in range (len(tokens)-2) : 
+        if tokens[i] == myList[0] and tokens[i+1] == myList[1] :
+            counter+=1
+            if tokens[i+2] in dic :
+                dic[tokens[i+2]]+=1
+            else:
+                dic[tokens[i+2]] = 1
+                
+    if tokens[(len(tokens))-2] == myList[0] and tokens[(len(tokens))-1] == myList[1]: 
+        counter+=1
+    return counter
+    
+
+def calculateProbability(dic,counter) :
+    if counter != 0 :
+        for key in dic :
+            dic[key] /= counter
+        
+        
+        
+mp = {}
+userInput = input()
+counter = getCounterOfWords(userInput,tokens,mp)
+calculateProbability(mp,counter)
+sorted_dict = sorted(mp, key=mp.get , reverse=True) 
+mn = min(len(sorted_dict),5)
+for i in range(mn) :
+    print(sorted_dict[i])
+    
+    
+    
+root = Tk()
+
+myLabel  = Label(root,text="Start Typing...",font = ("Helvetica",14),fg = "grey")
+myLabel.pack(pady=20)
+
+myEntry = Entry(root,font = ("Helvetica",20))
+myEntry.pack()
+
+myList = Listbox(root,width = 50)
+myList.pack(pady = 40)
+
+
+
+root.mainloop()
+
+ 
